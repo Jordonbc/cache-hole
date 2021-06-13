605235ffd8f67be9b819d14ef6d49bf2f888d599
Jordonbc <jordonbc@hotmail.co.uk>
2021-06-13 21:25:38 +0100

    update files

--- cache-install.sh
+++ cache-install.sh
@@ -1,7 +1,8 @@
+#!/bin/bash -e
 apt-get update
 apt-get install --no-install-recommends -y git jq
 
 git clone https://github.com/Jordonbc/cache-domains.git /CacheDomains
 #echo "{'ips': {'generic':	'${LANCACHE_IP}'},'cache_domains': {'default': 	'generic','blizzard': 	'generic','origin': 	'generic','riot': 	'generic','steam': 	'generic','wsus': 	'generic','xboxlive': 	'generic'}}" > /CacheDomains/scripts/config.json
 
-sh /cache-update.sh
+bash /cache-update.sh
