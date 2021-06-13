2c09d11149fafcb995b51e791b5fbed84f528280
Jordonbc <jordonbc@hotmail.co.uk>
2021-06-13 20:42:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -174,9 +174,9 @@ fi
 apt-get update
 apt-get install --no-install-recommends -y git jq
 
-git clone https://github.com/Jordonbc/cache-domains.git CacheDomains
+git clone https://github.com/Jordonbc/cache-domains.git /CacheDomains
 #echo "{'ips': {'generic':	'${LANCACHE_IP}'},'cache_domains': {'default': 	'generic','blizzard': 	'generic','origin': 	'generic','riot': 	'generic','steam': 	'generic','wsus': 	'generic','xboxlive': 	'generic'}}" > /CacheDomains/scripts/config.json
-cd CacheDomains/scripts
+cd /CacheDomains/scripts
 bash create-dnsmasq.sh
 
 mkdir -p /etc/dnsmasq/
