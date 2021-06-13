fa5102389e87a56fa066af8797ccb36c7e440b7a
Jordonbc <jordonbc@hotmail.co.uk>
2021-06-13 22:03:27 +0100

    Update cache-install.sh

--- cache-install.sh
+++ cache-install.sh
@@ -4,5 +4,13 @@ apt-get install --no-install-recommends -y git jq
 
 git clone https://github.com/Jordonbc/cache-domains.git /CacheDomains
 #echo "{'ips': {'generic':	'${LANCACHE_IP}'},'cache_domains': {'default': 	'generic','blizzard': 	'generic','origin': 	'generic','riot': 	'generic','steam': 	'generic','wsus': 	'generic','xboxlive': 	'generic'}}" > /CacheDomains/scripts/config.json
+cd /CacheDomains/scripts
 
-bash /cache-update.sh
+./create-dnsmasq.sh
+sudo cp -rf ./output/dnsmasq/*.conf /etc/dnsmasq.d
+sudo cp -rf ./output/dnsmasq/hosts/* /etc/dnsmasq/
+
+echo "Restarting Pi-hole DNS..."
+pihole restartdns
+
+echo "Done!"
