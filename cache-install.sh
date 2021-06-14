7674bb58a93ef90a02407f4bbfaa0fe735b48a95
Jordonbc <jordonbc@hotmail.co.uk>
2021-06-14 02:46:27 +0100

    added support for multiple ip addresses to be given

--- cache-install.sh
+++ cache-install.sh
@@ -11,7 +11,8 @@ git clone ${CACHE_DOMAINS_REPO} /CacheDomains
 cd /CacheDomains/scripts
 
 echo "Replacing ips in config.json with ${LANCACHE_IP}"
-sed -i "s/ipaddress/${LANCACHE_IP}/g" config.json
+#sed -i "s/ipaddress/${LANCACHE_IP}/g" config.json
+sed "s/\[.*\]/[${LANCACHE_IP}]/g" config.json
 
 echo "config.json now contains: "
 cat config.json
