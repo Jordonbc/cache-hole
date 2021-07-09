89ae9a45cacd44aee735a416379a50f05a53b452
Jordonbc <jordonbc@hotmail.co.uk>
2021-07-09 02:44:35 +0100

    Revert "Update cache-install.sh"
    This reverts commit 60867fb222235b1a2091ff9e642b62786b30ef19.

--- cache-install.sh
+++ cache-install.sh
@@ -8,13 +8,12 @@ apt-get install --no-install-recommends -y git jq
 echo "Cloning ${CACHE_DOMAINS_REPO} to /CacheDomains"
 git clone ${CACHE_DOMAINS_REPO} /CacheDomains
 
-cd /CacheDomains/scripts
-
 echo "Switching branch to ${CACHE_DOMAINS_BRANCH}"
 git checkout ${CACHE_DOMAINS_BRANCH}
 echo "Switch Complete"
 
 #echo "{'ips': {'generic':	'${LANCACHE_IP}'},'cache_domains': {'default': 	'generic','blizzard': 	'generic','origin': 	'generic','riot': 	'generic','steam': 	'generic','wsus': 	'generic','xboxlive': 	'generic'}}" > /CacheDomains/scripts/config.json
+cd /CacheDomains/scripts
 
 echo "Replacing ips in config.json with ${LANCACHE_IP}"
 #sed -i "s/ipaddress/${LANCACHE_IP}/g" config.json
